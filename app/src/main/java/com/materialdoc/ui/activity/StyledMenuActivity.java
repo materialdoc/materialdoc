8d415a69521b53ddaf681b4d2ecfcaa5cb7e40f1
Dmytro Danylyk <dmytrodanylyk@gmail.com>
2016-01-31 14:40:42 +0200

    Fixed issue with styled menu

--- app/src/main/java/com/materialdoc/ui/activity/StyledMenuActivity.java
+++ app/src/main/java/com/materialdoc/ui/activity/StyledMenuActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 
 import com.materialdoc.R;
@@ -20,6 +21,7 @@ public class StyledMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ac_menu_styled);
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
     }
 
     @Override
